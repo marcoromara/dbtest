bf7cdd5bc91d4d6595265a3a49d38469b57d5bd4
Romara <macroromara@gmail.com>
2018-09-05 12:37:13 +0300

    creating

---
+++ file.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div>
+		<h1>National institute of transport</h1>
+		<div>
+			<ul>
+				<li><a href="#">home</a></li>
+				<li><a href="#">home</a></li>
+				<li><a href="#">home</a></li>
+				<li><a href="#">home</a></li>
+				<li><a href="#">home</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html>
